342218785e7f76f127082546d68b92067f5de9fc
Aka Kherkeladze <akaki.kherkeladze@gmail.com>
2016-05-30 23:35:31 +0400

    fix aws parameter name, add parameters description

--- index.js
+++ index.js
@@ -3,74 +3,56 @@
 const aws = require('aws-sdk');
 
 class Mutex {
-
     /**
      * Mutex class constructor
-     * @param {Object} opts configuration parameters
+     * @param {Object} options configuration parameters
      *
-     opts : {
-        region : 'REGION',
-        accessKeyId : 'ACCESS_KEY_ID',
-        accessKey : 'ACCESS_KEY',
-        retryInterval : INTERVAL_TIME,
-        tableName : 'TABLE_NAME'
-     }
+     * options: {
+   *  region :            AWS region
+   *  accessKeyId:        AWS access key ID
+   *  secretAccessKey:    AWS secret access key
+   *                      (AWS parameters are optional and come from aws.config if missed)
+   *
+   *  lockRetryInterval:  lock retry interval (optional, 1000ms by default)
+   *  lockTimeout:        lock timeout (optional, 10000ms by default)
+   *  tableName:          mutex table name (optional, 'mutex-table' by default)
+   * }
      */
-    constructor(opts) {
-        this._db = new aws.DynamoDB(opts);
-        this._dbc = new aws.DynamoDB.DocumentClient(opts);
-        this._tableName = opts.tableName || 'mutex-table';
-        this._retryIntervalTime = opts.retryInterval || 1000;
+    constructor(options) {
+        if (!options) options = {};
+
+        this._db = new aws.DynamoDB(options);
+        this._dbc = new aws.DynamoDB.DocumentClient(options);
+        this._tableName = options.tableName || 'mutex-table';
+
+        this._lockTimeout = options.lockTimeout || 10000;
+        this._lockRetryInterval = options.lockRetryInterval || 1000;
 
         //check if mutex table exists and create one if does not
-        this._db.describeTable({ TableName : this._tableName }, err => {
-            if(err && err.code === 'ResourceNotFoundException') {
+        this._db.describeTable({ TableName: this._tableName }, err => {
+            if (err && err.code === 'ResourceNotFoundException') {
+                console.log('creating', this._tableName, ' table...');
+
                 this._db.createTable({
-                    TableName : this._tableName,
-                    AttributeDefinitions : [
-                        { AttributeName : 'key', AttributeType : 'S' }
+                    TableName: this._tableName,
+
+                    AttributeDefinitions: [
+                        { AttributeName: 'key', AttributeType: 'S' }
                     ],
-                    KeySchema : [
-                        { AttributeName : 'key', KeyType : 'HASH' }
+
+                    KeySchema: [
+                        { AttributeName: 'key', KeyType: 'HASH' }
                     ],
+
+                    //probably need to make this configurable as well...
                     ProvisionedThroughput: {
                         ReadCapacityUnits: 1,
                         WriteCapacityUnits: 1
                     }
-                }, (err) => err);
+                }, () => {
+                    console.log(this._tableName, 'created');
+                });
             }
-        });
-    }
-
-    /**
-     * Write Lock
-     * @access private
-     * @param {string} key
-     * @param {number} timeout
-     * @param {number} retryInterval
-     * @param {function} callback
-     */
-    _writeLock(key, timeout, retryInterval, callback) {
-      let now = Date.now();
-      let item = {
-          key : key,
-          expire : now + timeout
-      };
-        this._dbc.put({
-            TableName : this._tableName,
-            Item: item,
-            ConditionExpression: '#key <> :key OR (#key = :key AND #expire < :expire)',
-            ExpressionAttributeNames: {
-                '#key': 'key',
-                '#expire': 'expire'
-            },
-            ExpressionAttributeValues: {
-                ':key': item.key,
-                ':expire': now
-            }
-        }, err => {
-            if(!err) clearInterval(retryInterval);
-            callback(!err);
         });
     }
 
@@ -78,35 +60,64 @@ class Mutex {
      * Lock
      * @access public
      * @param {string} key
-     * @param {number} timeout
-     * @param {function} callback
+     * @param {number} timeout (optional)
+     * @param {function} action
      */
-    lock(key, timeout, callback) {
-        if(typeof timeout === 'function') {
-            callback = timeout;
-            timeout = this._retryIntervalTime;
+    lock(key, timeout, action) {
+        if (typeof timeout === 'function') {
+            action = timeout;
+            timeout = this._lockTimeout;
         }
-        let retryInterval = setInterval( () => {
-            this._writeLock(key, timeout, retryInterval, (success) => {
-                if(success) callback(() => this._unlock(key));
-            });
 
-        }, this._retryIntervalTime);
+        console.log('acquiring lock for', key);
+
+        let retryLock = setInterval(() => {
+            this._writeLock(key, timeout, (success) => {
+                if (success) {
+                    console.log(key, 'locked');
+                    clearInterval(retryLock);
+                    action(() => this._unlock(key));
+                }
+            });
+        }, this._lockRetryInterval);
     }
 
-    /**
-     * Unlock
-     * @access private
-     * @param {string} key
-     */
+    _writeLock(key, timeout, callback) {
+        let now = Date.now();
+
+        this._dbc.put({
+            TableName: this._tableName,
+
+            Item: {
+                key: key,
+                expire: now + timeout
+            },
+
+            ConditionExpression: '#key <> :key OR (#key = :key AND #expire < :expire)',
+
+            ExpressionAttributeNames: {
+                '#key': 'key',
+                '#expire': 'expire'
+            },
+
+            ExpressionAttributeValues: {
+                ':key': key,
+                ':expire': now
+            }
+        }, err => {
+            callback(!err);
+        });
+    }
+
     _unlock(key) {
         let params = {
-            TableName : this._tableName,
-            Key: {
-                key: key
-            }
+            TableName: this._tableName,
+            Key: { key: key }
         };
-        this._dbc.delete(params, err => err);
+
+        this._dbc.delete(params, () => {
+            console.log(key, 'unlocked');
+        });
     }
 }
 
